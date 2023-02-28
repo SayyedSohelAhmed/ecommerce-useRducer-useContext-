d20949e1e2b2ac84ce80071e647d352938f86558
mohammedasim7020 <asimferoz555@gmail.com>
2023-02-28 23:53:54 +0530

    second commit

--- src/component/Some.jsx
+++ src/component/Some.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 export const Some = () => {
   return (
     <>
-    
-    <div>Some</div>
-    <div>asim</div>
-    <div>Some</div>
+      <p>jdhfkjfgbv</p>
+      <div>Some</div>
+      <div>asim</div>
+      <div>Some</div>
+      <div>Sohel</div>
     </>
-      )
-    }
+  )
+}
